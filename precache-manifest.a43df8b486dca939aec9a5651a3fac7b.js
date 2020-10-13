dc4862269e61f5db4cf655968b2296f531c1a708
karmelyoei <karmel.salah@gmail.com>
2020-10-13 22:01:58 +0300

    Updates

---
+++ precache-manifest.a43df8b486dca939aec9a5651a3fac7b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bee2855adb04faf9fd0e122f69c668d4",
+    "url": "/React-Monsters/index.html"
+  },
+  {
+    "revision": "d6f117a00162769fee6c",
+    "url": "/React-Monsters/static/css/main.fc1b0864.chunk.css"
+  },
+  {
+    "revision": "78adac64eaa07ff35dea",
+    "url": "/React-Monsters/static/js/2.356d906f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Monsters/static/js/2.356d906f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6f117a00162769fee6c",
+    "url": "/React-Monsters/static/js/main.4033f956.chunk.js"
+  },
+  {
+    "revision": "2668bc56a8f69c8729e2",
+    "url": "/React-Monsters/static/js/runtime-main.9151ee87.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/React-Monsters/precache-manifest.a43df8b486dca939aec9a5651a3fac7b.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/React-Monsters/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
